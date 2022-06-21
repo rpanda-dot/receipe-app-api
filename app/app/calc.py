9093575041c1fef20fc2928de318eb57ea30b8c4
Rajaram Panda <rpanda@argusoft.in>
2022-06-21 17:32:19 +0530

    changes added postgres

---
+++ app/app/calc.py
@@ -0,0 +1,7 @@
+"""
+Calculator Function
+"""
+
+def add(x,y):
+    """Add x and y and return result"""
+    return x+y
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from . import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the cac module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+
