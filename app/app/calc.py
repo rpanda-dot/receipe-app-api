41bc9718974d9a7aa90f2aa77ccf217c8c8f3f31
Rajaram Panda <rpanda@argusoft.in>
2022-06-21 17:59:56 +0530

    linging issue

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,6 +2,7 @@
 Calculator Function
 """
 
+
 def add(x,y):
     """Add x and y and return result"""
     return x+y
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,13 +6,15 @@ from django.test import SimpleTestCase
 
 from . import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the cac module."""
+
 
     def test_add_numbers(self):
         """Test adding numbers together."""
 
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
